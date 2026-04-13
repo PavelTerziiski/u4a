378b3ac86c56f15fffb6ccb76b6a57872e11904a
PavelTerziiski <pavel.sings@gmail.comgit config --global user.email pavel.impro@gmail@gmail.com>
2026-04-13 11:40:41 +0300

    fix: mix-blend-mode lighten

--- src/components/fox/Fox.tsx
+++ src/components/fox/Fox.tsx
@@ -45,7 +45,7 @@ export default function Fox({ mood = 'happy', size = 160, name }: FoxProps) {
             loop
             muted
             playsInline
-            style={{ width: size, height: size, objectFit: 'contain', mixBlendMode: 'multiply' }}
+            style={{ width: size, height: size, objectFit: 'contain', mixBlendMode: 'lighten' }}
           />
         ) : (
           <img
