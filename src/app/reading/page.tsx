fe23f433ae33c2cd4dd5c23685be9e89de46ddbc
PavelTerziiski <pavel.sings@gmail.comgit config --global user.email pavel.impro@gmail@gmail.com>
2026-04-29 12:36:41 +0300

    fix: disable silence detection, no emoji in feedback

--- src/app/reading/page.tsx
+++ src/app/reading/page.tsx
@@ -96,7 +96,7 @@ export default function ReadingPage() {
       }
       silenceTimerRef.current = setTimeout(checkSilence, 200)
     }
-    setTimeout(checkSilence, 1000)
+    // silence detection disabled
     setTimeout(() => { if (mediaRecorderRef.current&&mediaRecorderRef.current.state === 'recording') handleRecord() }, 10000)
   }
 
